92253ac8ec0adadfa88d797d120405ee75fa5615
fawahkhan <khanmohdfawah21@gmail.com>
2026-02-01 23:57:52 +0530

    Updated index.js

--- server/index.js
+++ server/index.js
@@ -1,36 +1,10 @@
 const express = require("express") ;
+const { userRouter } = require("./routes/user");
+const { courseRouter } = require("./routes/course");
 
 const app = express()
 app.use(express.json)
 
-app.post('/user/signup', function(req,res){
-    const name = req.body.name ;
-    const password = req.body.password ;
-    const email = req.body.email ;
-    res.json({
-        msg: "Signed up"
-    })
-})
-app.post('/user/signin', function(req,res){
-    res.json({
-        msg:"User login"
-    })
-})
-app.post('/buy', function(req,res){
-    //you wouuld expect the user to pay money here.
-    res.json({
-        msg:"Bought the course"
-    })
-})
-//these two need authentication
-app.get('/courses', function(req,res){
-    res.json({
-        courses
-    })
-})
-app.get('/user/purchases', function(req,res){
-    res.json({
-        PurchasedCourses
-    })
-})
+app.use("/user", userRouter)
+app.use("/course", courseRouter)
 app.listen(3000)
