cdfcdae3b57fcfc6a4f722906be3bd1f082fe786
fawahkhan <khanmohdfawah21@gmail.com>
2026-02-02 00:04:35 +0530

    Fixed an endpt

--- server/routes/course.js
+++ server/routes/course.js
@@ -4,14 +4,14 @@ const courseRouter = Router();
 
 
 //to buy a cours
-courseRouter.post('/course/purchase', function(req,res){
+courseRouter.post('/purchase', function(req,res){
     //you wouuld expect the user to pay money here.
     res.json({
         msg:"Bought the course"
     })
 })
 //to view all courses
-courseRouter.get('/course/preview', function(req,res){
+courseRouter.get('/preview', function(req,res){
     res.json({
         courses
     })
