6c9627ec812ede42230f11e5a8b5660e62fef2e4
fawahkhan <khanmohdfawah21@gmail.com>
2026-02-03 08:04:58 +0530

    Added user Signup endpoint and fixed admin signup

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,22 +1,37 @@
+const bcrypt = require("bcrypt")
 const {Router} = require ("express")
 
 const userRouter = Router();
+const {UserModel} = require('../db') 
 
+userRouter.post('/signup', async function(req,res){
+    try {
+        const {email, password, firstName, lastname} = req.body    //adding zod validation is left 
+        const hashedPassword = await bcrypt.hash(password, 5) 
+          
+        await UserModel.create({
+        email: email ,
+        password: hashedPassword, //rather than storing the password directly here we will be storingg the hashed password created by bcrypt.
+        firstName : firstName, 
+        lastname: lastname
+        })
 
-userRouter.post('/user/signup', function(req,res){
-    const name = req.body.name ;
-    const password = req.body.password ;
-    const email = req.body.email ;
-    res.json({
-        msg: "Signed up"
+        res.json({
+        msg: "User Signed up successfully"
     })
+    }catch(e){
+        res.status(500).json({
+            msg: "ERROR while signup"
+        })
+    }
+    
 })
-userRouter.post('/user/signin', function(req,res){
+userRouter.post('/signin', function(req,res){
     res.json({
         msg:"User login"
     })
 })
-userRouter.get('/user/purchases', function(req,res){
+userRouter.get('/purchases', function(req,res){
     res.json({
         PurchasedCourses
     })
