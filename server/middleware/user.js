550f16c82eaa725f33c2a81d03291857eb25119a
fawahkhan <khanmohdfawah21@gmail.com>
2026-02-17 22:03:31 +0530

    INitialised middleware for user

---
+++ server/middleware/user.js
@@ -0,0 +1,15 @@
+const jwt = require("jsonwebtoken")
+const {JWT_USER_PASSWORD} = require ("../config")
+
+function userMiddleware(req , res , next){
+    const token = req.headers.token ;
+    const decoded = jwt.verify(token , JWT_USER_PASSWORD)
+
+    if (decoded ){
+        req.userID = decoded.id
+    }
+}
+
+module.exports ={
+    userMiddleware: userMiddleware  
+}
