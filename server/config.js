75de3491ce6a57be8ca901cc1ab2239cd1484573
fawahkhan <khanmohdfawah21@gmail.com>
2026-02-17 23:31:17 +0530

    Edited config

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
-JWT_USER_PASSWORD ="useronly123"  //for signing users , admin passwords would be signed by their own passwords which is different
-JWT_ADMIN_PASSWORD ="adminonly123"  //for signing admins
+JWT_USER_PASSWORD =process.env.JWT_USER_PASSWORD  //for signing users , admin passwords would be signed by their own passwords which is different
+JWT_ADMIN_PASSWORD =process.env.JWT_ADMIN_PASSWORD  //for signing admins
 
 module.exports = {
     JWT_USER_PASSWORD,
