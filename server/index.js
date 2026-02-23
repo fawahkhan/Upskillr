853b7d0af9869c6fdc9440150794913b46625737
fawahkhan <khanmohdfawah21@gmail.com>
2026-02-23 19:54:21 +0530

    Enabled cors

--- server/index.js
+++ server/index.js
@@ -1,10 +1,18 @@
 require('dotenv').config()
 const express = require("express") ;
+const cors = require("cors");
 const { userRouter } = require("./routes/user");
 const { courseRouter } = require("./routes/course");
 const { adminRouter } = require("./routes/admin");
 const mongoose = require('mongoose');
 const app = express()
+
+// Enable CORS for frontend
+app.use(cors({
+    origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175'],
+    credentials: true
+}));
+
 app.use(express.json()) 
 
 app.use("/api/v1/user", userRouter)
