5a0e1b80feb91f76a6ac990e90fa26576e12f507
fawahkhan <khanmohdfawah21@gmail.com>
2026-02-26 20:43:32 +0530

    Cors connection string edited

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const app = express()
 
 // Enable CORS for frontend
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175'],
+    origin: ['https://upskillr-kappa.vercel.app'],
     credentials: true
 }));
 
