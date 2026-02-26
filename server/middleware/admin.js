babe326fd8a1bbec38d82677a29b7cd9aec61dfd
fawahkhan <khanmohdfawah21@gmail.com>
2026-02-26 17:55:12 +0530

    Both middleware files are fixed and authorized

--- server/middleware/admin.js
+++ server/middleware/admin.js
@@ -2,17 +2,18 @@ const jwt = require("jsonwebtoken")
 const {JWT_ADMIN_PASSWORD} = require ("../config")
 
 function adminMiddleware(req , res , next){
-    const token = req.headers.token ;
-    const decoded = jwt.verify(token , JWT_ADMIN_PASSWORD)
-
-    if (decoded ){
-        // when we encoded in signin of admin , an id field was created we will be using this key called id in our middlewares
-        req.adminID = decoded.id 
-        next()
-    }else{
-        res.status(403).json({
-            msg : "YOU are not signed in"
-        })
+    try {
+        const authHeader = req.headers.authorization || req.headers.token;
+        const token = authHeader?.startsWith('Bearer ') ? authHeader.slice(7) : authHeader;
+        const decoded = jwt.verify(token , JWT_ADMIN_PASSWORD)
+        if (decoded) {
+            req.adminID = decoded.id
+            next()
+        } else {
+            res.status(403).json({ msg : "YOU are not signed in" })
+        }
+    } catch(e) {
+        res.status(403).json({ msg : "YOU are not signed in" })
     }
 }
 
--- server/middleware/user.js
+++ server/middleware/user.js
@@ -2,16 +2,18 @@ const jwt = require("jsonwebtoken")
 const {JWT_USER_PASSWORD} = require ("../config")
 
 function userMiddleware(req , res , next){
-    const token = req.headers.token ;
-    const decoded = jwt.verify(token , JWT_USER_PASSWORD)
-
-    if (decoded ){
-        req.userID = decoded.id
-        next()
-    }else{
-        res.status(403).json({
-            msg : "You are not signed in"
-        })
+    try {
+        const authHeader = req.headers.authorization || req.headers.token;
+        const token = authHeader?.startsWith('Bearer ') ? authHeader.slice(7) : authHeader;
+        const decoded = jwt.verify(token , JWT_USER_PASSWORD)
+        if (decoded) {
+            req.userID = decoded.id
+            next()
+        } else {
+            res.status(403).json({ msg : "You are not signed in" })
+        }
+    } catch(e) {
+        res.status(403).json({ msg : "You are not signed in" })
     }
 }
 
